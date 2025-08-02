f7224cd1f4d32228414cb109971779198c73ea10
Harsh Dadiya <harshdadiya@gmail.com>
2025-08-02 19:56:28 +0530

    Learned basics how to compile and run basic cpp program..

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*cout << "Hello World" << endl;
+    return 0;*/
+    cout << "Hello, World!" << endl;
+    cout << "This is a simple C++ program." << endl;
+    cout << "C++20 is a powerful language." << endl;
+    cout << "Let's explore its features!" << endl;
+    cout << "Happy coding!" << endl;
+    return 0;
+}
